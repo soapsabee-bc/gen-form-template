6b4491923f407883ac8fb0bcfaadf23d22113940
soapsabee-bc <patiphan.soap@bcircle.co.th>
2021-03-20 01:45:33 +0700

    include template

--- index.js
+++ index.js
@@ -41,12 +41,16 @@ let test = initTemplate(
 { name: "test123" },[
 template.container(
     [ 
-        [ template.textFieldCustom({id:"test",name:"test"}),template.textFieldCustom({id:"test",name:"test"}) ],
-    
-    ]) 
+        [ template.textFieldCustom({id:"test",name:"test",required:true}),template.textFieldCustom({id:"test",name:"test"}) ],
+       
+    ]) ,
+    template.container(
+        [ 
+            [ template.tableTwo({id:"test2",name:"test2"})],
+           
+        ]) 
 
 ])
-
 const jsonString = JSON.stringify(test)
 fs.writeFile('./newCustomer.json', jsonString, err => {
     if (err) {
--- template.js
+++ template.js
@@ -32,9 +32,9 @@ const container = (fieldArray) => {
         col: -1,
         visibilityCondition: null,
         numberOfColumns: fieldArray.length,
-        fields:{...fieldArray}
+        fields: { ...fieldArray }
     }
-    
+
     return object1
 }
 
@@ -45,7 +45,7 @@ const textFieldCustom = (prop) => {
         name: prop.name,
         type: "text_input_custom",
         value: null,
-        required: false,
+        required: false || prop.required,
         readOnly: false,
         overrideId: false,
         colspan: 1,
@@ -84,4 +84,192 @@ const textFieldCustom = (prop) => {
     return textField
 }
 
-module.exports = { container , textFieldCustom };
+const tableTwo = (prop) => {
+    let objectTable = {
+        fieldType: "FormFieldRepresentation",
+        id: prop.id,
+        name: prop.name,
+        type: "table",
+        value: null,
+        required: false,
+        readOnly: false,
+        overrideId: false,
+        colspan: 1,
+        placeholder: null,
+        minLength: 0,
+        maxLength: 0,
+        minValue: null,
+        maxValue: null,
+        regexPattern: null,
+        optionType: null,
+        hasEmptyValue: null,
+        options: null,
+        restUrl: null,
+        restResponsePath: null,
+        restIdProperty: null,
+        restLabelProperty: null,
+        tab: null,
+        className: null,
+        params: {
+            existingColspan: 1,
+            maxColspan: 2
+        },
+        dateDisplayFormat: null,
+        layout: {
+            row: -1,
+            column: -1,
+            colspan: 1
+        },
+        sizeX: 1,
+        sizeY: 1,
+        row: -1,
+        col: -1,
+        visibilityCondition: null
+    };
+    return objectTable
+}
+
+const dropdown = (prop) => {
+    let objectDropdown = {
+        fieldType: "RestFieldRepresentation",
+        id: prop.id,
+        name: prop.name,
+        type: "dropdown",
+        value: "Choose one...",
+        required: false || prop.required,
+        readOnly: false,
+        overrideId: false,
+        colspan: 1,
+        placeholder: null,
+        minLength: 0,
+        maxLength: 0,
+        minValue: null,
+        maxValue: null,
+        regexPattern: null,
+        optionType: null,
+        hasEmptyValue: true,
+        options: [
+            {
+                id: "empty",
+                name: "Choose one..."
+            },
+            ...prop.options
+        ],
+        restUrl: null,
+        restResponsePath: null,
+        restIdProperty: null,
+        restLabelProperty: null,
+        tab: null,
+        className: null,
+        params: {
+            existingColspan: 1,
+            maxColspan: 2
+        },
+        dateDisplayFormat: null,
+        layout: {
+            row: -1,
+            column: -1,
+            colspan: 1
+        },
+        sizeX: 1,
+        sizeY: 1,
+        row: -1,
+        col: -1,
+        visibilityCondition: null,
+        endpoint: null,
+        requestHeaders: null
+    }
+    return objectDropdown
+}
+
+const date = (prop) => {
+    let objectDate = {
+        fieldType: "FormFieldRepresentation",
+        id: prop.id,
+        name: prop.name,
+        type: "date",
+        value: null,
+        required: false || prop.required,
+        readOnly: false,
+        overrideId: false,
+        colspan: 1,
+        placeholder: null,
+        minLength: 0,
+        maxLength: 0,
+        minValue: null,
+        maxValue: null,
+        regexPattern: null,
+        optionType: null,
+        hasEmptyValue: null,
+        options: null,
+        restUrl: null,
+        restResponsePath: null,
+        restIdProperty: null,
+        restLabelProperty: null,
+        tab: null,
+        className: null,
+        params: {
+            existingColspan: 1,
+            maxColspan: 2
+        },
+        dateDisplayFormat: null,
+        layout: {
+            row: -1,
+            column: -1,
+            colspan: 1
+        },
+        sizeX: 1,
+        sizeY: 1,
+        row: -1,
+        col: -1,
+        visibilityCondition: null
+    }
+
+    return objectDate
+}
+
+const header = (prop,fieldArray) => {
+    let objectHeader = {
+        fieldType: "ContainerRepresentation",
+        id: prop.id,
+        name: prop.name,
+        type: "group",
+        value: null,
+        required: false || prop.required,
+        readOnly: false,
+        overrideId: false,
+        colspan: 1,
+        placeholder: null,
+        minLength: 0,
+        maxLength: 0,
+        minValue: null,
+        maxValue: null,
+        regexPattern: null,
+        optionType: null,
+        hasEmptyValue: null,
+        options: null,
+        restUrl: null,
+        restResponsePath: null,
+        restIdProperty: null,
+        restLabelProperty: null,
+        tab: null,
+        className: null,
+        dateDisplayFormat: null,
+        layout: {
+            row: -1,
+            column: -1,
+            colspan: 2
+        },
+        sizeX: 2,
+        sizeY: 1,
+        row: -1,
+        col: -1,
+        visibilityCondition: null,
+        numberOfColumns: fieldArray.length,
+        fields: { ...fieldArray }
+    }
+
+    return objectHeader
+}
+
+module.exports = { container, textFieldCustom, tableTwo, dropdown, date , header };
